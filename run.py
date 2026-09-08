9188836e56be999407f6717a13b7cdfc9071118f
Dev-vesper <vesper.oneold@gmail.com>
2026-09-08 04:43:05 +0330

    wire world, camera and menu into run

--- run.py
+++ run.py
@@ -1,1 +1,21 @@
-# run game
+import json
+
+from direct.showbase.ShowBase import ShowBase
+
+from src.core.controller.camera import CameraController
+from src.core.world.loader import WorldLoader
+from src.ui.windows import StartWindow
+
+if __name__ == '__main__':
+    base = ShowBase()
+
+    with open('assets/worlds/test_map.json') as file:
+        blocks = json.load(file)
+    world = WorldLoader(blocks)
+    world.load()
+
+    base.world = world
+    base.camera_controller = CameraController(focus=world.center)
+    base.start_menu = StartWindow()
+
+    base.run()
